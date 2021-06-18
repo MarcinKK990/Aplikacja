63a0859ca20f54e4b223b540bc8a141610deb78d
MarcinKK990 <72249735+MarcinKK990@users.noreply.github.com>
2021-06-18 13:35:24 +0200

    Radio buttons

--- src/components/Slusarz.js
+++ src/components/Slusarz.js
@@ -5,6 +5,8 @@ function Slusarz() {
   const [price, setPrice] = useState(160);
   const [city, setCity] = useState();
   const [service, setService] = useState("Dorabianie kluczy");
+  const [choose, setChoose] = useState("Ja");
+  const [radio, setRadio] = useState("apple");
 
   useEffect(() => {
     fetch("http://localhost:3000/Slusarze")
@@ -18,64 +20,86 @@ function Slusarz() {
     setService(e.target.value);
   };
 
+  const handleOnChange = (e) => {
+    setChoose(e.target.value);
+  };
+
   return (
     <>
-      <form className="form_e">
-        <div class="form_Elektryk">
-          <div>
-            {" "}
-            <h3> Wybierz rodzaj usługi</h3>{" "}
-          </div>
-          <div className="form">
-            <div>
-              {data.length ? (
-                <select value={service} onChange={handleOptionChange}>
-                  {data.map((el) => {
-                    return <option>{el.usługa}</option>;
-                  })}
-                </select>
-              ) : (
-                <p>loading..</p>
-              )}
+      <div className="kkk">
+        <div className="aaa">
+          <form className="form_e">
+            <div class="form_Elektryk">
+              <div>
+                {" "}
+                <h3> {radio} </h3>{" "}
+              </div>
+              <div className="form">
+                <div>
+                  {data.length ? (
+                    <select value={service} onChange={handleOptionChange}>
+                      {data.map((el) => {
+                        return <option>{el.usługa}</option>;
+                      })}
+                    </select>
+                  ) : (
+                    <p>loading..</p>
+                  )}
+                </div>
+              </div>
+              <div>
+                <h3> Podaj swój budżet </h3>
+                <form>
+                  <input
+                    type="number"
+                    value={price}
+                    onChange={(e) => {
+                      setPrice(e.target.value);
+                    }}
+                  />
+                  <h3> Podaj swoją lokalizację</h3>
+                  <input
+                    type="text"
+                    value={city}
+                    onChange={(e) => {
+                      setCity(e.target.value);
+                    }}
+                  />
+                </form>
+              </div>
             </div>
-          </div>
-          <div>
-            <h3> Podaj swój budżet </h3>
-            <form>
-              <input
-                type="number"
-                value={price}
-                onChange={(e) => {
-                  setPrice(e.target.value);
-                }}
-              />
-              <h3> Podaj swoją lokalizację</h3>
-              <input
-                type="text"
-                value={city}
-                onChange={(e) => {
-                  setCity(e.target.value);
-                }}
-              />
-            </form>
-          </div>
+          </form>
         </div>
-      </form>
-      <div className="a">
-        <ul className="filter">
-          {data
-            .filter(
-              (el) =>
-                el.price <= price && el.miasto === city && el.usługa === service
-            )
-            .map((el) => {
-              return (
-                <li>
-                  {el.name}, {el.price} zł, {el.usługa} {el.miasto}
-                </li>
-              );
-            })}
-        </ul>
+        <div className="a">
+          <form className="filter">
+            {data
+              .filter(
+                (el) =>
+                  el.price <= parseFloat(price) &&
+                  el.miasto === city &&
+                  el.usługa === service
+              )
+              .map((el) => {
+                return (
+                  <>
+                    <label key="index">
+                      {el.name}, {el.price} zł, {el.usługa} {el.miasto}
+                    </label>
+                    <input
+                      type="radio"
+                      checked={
+                        radio === el.name && el.price && el.usługa && el.miasto
+                      }
+                      value={(el.name, el.price, el.usługa, el.miasto)}
+                      onChange={(e) => {
+                        setRadio(e.target.value);
+                      }}
+                    />
+                  </>
+                );
+              })}
+          </form>
+        </div>
       </div>
     </>
   );
